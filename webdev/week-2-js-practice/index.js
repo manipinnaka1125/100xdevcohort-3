ee6a42f1e2ca75e5818b11a7a7445b2c6094fa02
p.maniswaroop <2200030449@kluniversity.in>
2024-08-18 09:48:45 +0530

    new data

---
+++ webdev/week-2-js-practice/index.js
@@ -0,0 +1,34 @@
+/*const fs=require("fs");
+function print (err,data){
+    if(err){
+        console.log("error occured");
+    }
+    else 
+    {
+        console.log(data);
+    }
+}
+fs.readFile("a.txt","utf-8",print);
+console.log("done");
+function dinner(){
+    console.log("i want to dinnner");
+}
+function cooking(){
+    console.log("i want to start cooking before eating the dinner");
+}
+console.log("after dinner after cooking i want to sleep");
+
+setTimeout(dinner,5000);
+setTimeout(cooking,6000);
+let c=0;
+for(let i=0;i<10000000000;i++){
+    c+=1;
+
+}
+console.log("hello today my daily task are completed");
+*/
+const user={
+    name:"maniswaroo",
+    age:21
+}
+console.log(user.name);
